08af257b996f15710bf420cd60c15e16da8d9f6f
Dmytro <git20121983@gmail.com>
2024-12-10 22:30:13 +0200

    post_list 0.1

--- cook/blog/urls.py
+++ cook/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
     path('', views.home),
 ]
--- cook/blog/views.py
+++ cook/blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from cook.blog.models import Post
+from blog.models import Post
+
+
 
 
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
+    
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs.get("slug"))
+    
 
 def home (request):
     return render(request, 'base.html')
